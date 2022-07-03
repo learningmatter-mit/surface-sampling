3445c12baac7ea57b04f5d25899dea9e648fdbd2
Xiaochen Du <dux@outlook.sg>
2022-07-03 18:59:11 -0400

    test Au complete

--- mcmc/__init__.py
+++ mcmc/__init__.py
@@ -1,1 +1,1 @@
-from .mcmc import initialize_slab, mcmc_run
+from .mcmc import get_adsorption_coords, initialize_slab, mcmc_run, slab_energy
--- tests/test_Au.py
+++ tests/test_Au.py
@@ -1,31 +1,52 @@
 import os
+import pickle
 import sys
 
+import catkit
 import numpy as np
 import pytest
 from ase.calculators.lammpsrun import LAMMPS
+from ase.io import read
+from catkit.gen.adsorption import get_adsorption_sites
 
-from mcmc import initialize_slab, mcmc_run
+from mcmc import get_adsorption_coords, mcmc_run
 
+current_dir = os.path.dirname(__file__)
+
+print(f"current folder is {current_dir}")
 
 # some regression testing first
-def test_Cu_energy():
-    required_energy = -47.30809647
+def test_Au_energy():
+    required_energy = -79.03490823689619
 
-    # initialize some parameters first
-    # Cu alat from https://www.copper.org/resources/properties/atomic_properties.html
-    Cu_alat = 3.6147
-    slab = initialize_slab(Cu_alat, size=(2, 2, 2))
+    # create slab and get proper ads sites
+    slab_pkl = open(
+        os.path.join(current_dir, "resources/Au_110_2x2_pristine_slab.pkl"), "rb"
+    )
+    slab = pickle.load(slab_pkl)
+
+    coords, connectivity, sym_idx = get_adsorption_sites(slab, symmetry_reduced=False)
+
+    element = "Au"
+    metal = catkit.gratoms.Gratoms(element)
+
+    ads_coords = get_adsorption_coords(slab, metal, connectivity)
+
+    proper_adsorbed = read(
+        os.path.join(current_dir, "resources/Au_110_2x2_proper_adsorbed_slab.cif")
+    )
+    ads_positions = proper_adsorbed.get_positions()[len(slab) :]
 
     chem_pot = 0  # chem pot 0 to less complicate things
-    alpha = 0.99  # slowly anneal
+    num_ads_atoms = 4 + 2  # for canonical runs
+    alpha = 0.9  # anneal
     temp = 1  # temp in terms of kbT
-    num_sweeps = 100
+    num_sweeps = 50
 
     # use LAMMPS
-    parameters = {"pair_style": "eam", "pair_coeff": ["* * Cu_u3.eam"]}
+    parameters = {"pair_style": "eam", "pair_coeff": ["* * Au_u3.eam"]}
 
-    potential_file = os.path.join(os.environ["LAMMPS_POTENTIALS"], "Cu_u3.eam")
+    potential_file = os.path.join(os.environ["LAMMPS_POTENTIALS"], "Au_u3.eam")
     lammps_calc = LAMMPS(
         files=[potential_file],
         keep_tmp_files=False,
@@ -37,11 +58,15 @@ def test_Cu_energy():
     # call the main function
     history, energy_hist, frac_accept_hist, adsorption_count_hist = mcmc_run(
         num_sweeps=num_sweeps,
-        temp=temp,
-        pot=chem_pot,
+        temp=1,
+        pot=0,
         alpha=alpha,
         slab=slab,
         calc=lammps_calc,
+        element=element,
+        canonical=True,
+        num_ads_atoms=num_ads_atoms,
+        ads_coords=ads_positions,
     )
 
     assert np.allclose(energy_hist[-1], required_energy)
