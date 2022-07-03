ff8f9dc0a0719ab4073b7253c9856e4a20cd330a
Xiaochen Du <dux@outlook.sg>
2022-07-03 18:57:02 -0400

    test Au

---
+++ tests/test_Au.py
@@ -0,0 +1,47 @@
+import os
+import sys
+
+import numpy as np
+import pytest
+from ase.calculators.lammpsrun import LAMMPS
+
+from mcmc import initialize_slab, mcmc_run
+
+
+# some regression testing first
+def test_Cu_energy():
+    required_energy = -47.30809647
+
+    # initialize some parameters first
+    # Cu alat from https://www.copper.org/resources/properties/atomic_properties.html
+    Cu_alat = 3.6147
+    slab = initialize_slab(Cu_alat, size=(2, 2, 2))
+
+    chem_pot = 0  # chem pot 0 to less complicate things
+    alpha = 0.99  # slowly anneal
+    temp = 1  # temp in terms of kbT
+    num_sweeps = 100
+
+    # use LAMMPS
+    parameters = {"pair_style": "eam", "pair_coeff": ["* * Cu_u3.eam"]}
+
+    potential_file = os.path.join(os.environ["LAMMPS_POTENTIALS"], "Cu_u3.eam")
+    lammps_calc = LAMMPS(
+        files=[potential_file],
+        keep_tmp_files=False,
+        keep_alive=False,
+        tmp_dir="/home/dux/surface_sampling/tmp_files",
+    )
+    lammps_calc.set(**parameters)
+
+    # call the main function
+    history, energy_hist, frac_accept_hist, adsorption_count_hist = mcmc_run(
+        num_sweeps=num_sweeps,
+        temp=temp,
+        pot=chem_pot,
+        alpha=alpha,
+        slab=slab,
+        calc=lammps_calc,
+    )
+
+    assert np.allclose(energy_hist[-1], required_energy)
