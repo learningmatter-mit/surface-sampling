9b6ffeb1e3afec6f8c36469bba857867afc22c3c
Xiaochen Du <dux@outlook.sg>
2022-10-05 20:37:30 -0400

    plot atoms

--- mcmc/plot.py
+++ mcmc/plot.py
@@ -2,6 +2,7 @@ import os
 
 import matplotlib.pyplot as plt
 import numpy as np
+from ase.visualize.plot import plot_atoms
 
 
 def plot_summary_stats(
@@ -29,3 +30,13 @@ def plot_summary_stats(
 
     fig.tight_layout()
     fig.savefig(os.path.join(title, "summary.png"))
+
+
+def visualize_two_slabs(slab1, slab2):
+    fig, ax = plt.subplots(1, 2, figsize=(10, 10))
+    labels = ["slab1", "slab2"]
+    for i in range(2):
+        ax[i].axis("off")
+        ax[i].set_title(labels[i])
+    plot_atoms(slab1, ax[0], radii=0.8, rotation=("90x, 15y, 90z"))
+    plot_atoms(slab2, ax[1], radii=0.8, rotation=("90x, 15y, 90z"))
