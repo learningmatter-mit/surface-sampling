3ddf996223b7d9c614ab15b3e4d83796b765dcfc
Xiaochen Du <dux@outlook.sg>
2024-05-21 22:33:59 -0400

    TST: update tests
    - new folders for static objects
    - pytest run tests align with new sampling code

--- tests/test_Au.py
+++ tests/test_Au.py
@@ -1,16 +1,14 @@
 import os
-import pickle
+import pickle as pkl
 
 import numpy as np
-from ase.calculators.lammpsrun import LAMMPS
 from ase.io import read
 
 from mcmc import MCMC
+from mcmc.calculators import LAMMPSRunSurfCalc
+from mcmc.system import SurfaceSystem
 
 current_dir = os.path.dirname(__file__)
-
-print(f"current folder is {current_dir}")
-
 
 # some regression testing first
 def test_Au_energy():
@@ -20,47 +18,80 @@ def test_Au_energy():
     slab_pkl = open(
         os.path.join(current_dir, "data/Au_110_2x2_pristine_slab.pkl"), "rb"
     )
-    slab = pickle.load(slab_pkl)
+
+    slab = pkl.load(slab_pkl)
 
     proper_adsorbed = read(
         os.path.join(current_dir, "data/Au_110_2x2_proper_adsorbed_slab.cif")
     )
     ads_positions = proper_adsorbed.get_positions()[len(slab) :]
-    sweep_size = len(ads_positions)
 
     element = "Au"
-    chem_pot = 0  # chem pot 0 to less complicate things
+
     num_ads_atoms = 4 + 2  # for canonical runs
-    alpha = 0.9  # anneal
-    temp = 1  # temp in terms of kbT
-    num_sweeps = 50
+
+    system_settings = {
+        "surface_name": "Au(110)",
+        "cutoff": 5.0,
+        "num_ads_atoms": num_ads_atoms,
+        "near_reduce": 0.01,
+        "planar_distance": 1.5,
+        "no_obtuse_hollow": True,
+    }
+
+    sampling_settings = {
+        "alpha": 0.99,  # slowly anneal
+        "temperature": 1.0,  # in terms of kbT
+        "num_sweeps": 50,
+        "sweep_size": len(ads_positions),
+    }
+
+    calc_settings = {
+        "calc_name": "eam",
+        "optimizer": "FIRE",
+        "chem_pots": {"Au": 0.0},
+        "relax_atoms": False,
+        "relax_steps": 100,
+        "run_dir": os.path.join(current_dir, "data/Au_110"),
+    }
 
     # use LAMMPS
     parameters = {"pair_style": "eam", "pair_coeff": ["* * Au_u3.eam"]}
 
+    # set up the LAMMPS calculator
     potential_file = os.path.join(os.environ["LAMMPS_POTENTIALS"], "Au_u3.eam")
-    lammps_calc = LAMMPS(
+    lammps_surf_calc = LAMMPSRunSurfCalc(
         files=[potential_file],
         keep_tmp_files=False,
         keep_alive=False,
+        tmp_dir=os.path.join(os.path.expanduser("~"), "tmp_files"),
     )
-    lammps_calc.set(**parameters)
+    lammps_surf_calc.set(**parameters)
 
-    # initialize object
-    Au_mcmc = MCMC(
-        calc=lammps_calc,
-        element=element,
+    # initialize SurfaceSystem
+    surface = SurfaceSystem(
+        slab, ads_positions, lammps_surf_calc, system_settings=system_settings
+    )
+
+    mcmc = MCMC(
+        system_settings["surface_name"],
+        calc=lammps_surf_calc,
         canonical=True,
-        num_ads_atoms=num_ads_atoms,
-        ads_coords=ads_positions,
-    )
-    Au_mcmc.mcmc_run(
-        total_sweeps=num_sweeps,
-        sweep_size=sweep_size,
-        start_temp=temp,
-        pot=chem_pot,
-        alpha=alpha,
-        slab=slab,
-    )  # call the main
+        testing=False,
+        element=element,
+        adsorbates=list(calc_settings["chem_pots"].keys()),
+        relax=calc_settings["relax_atoms"],
+        optimizer=calc_settings["optimizer"],
+        num_ads_atoms=system_settings["num_ads_atoms"],
+    )  # no relaxation
 
-    assert np.allclose(np.min(Au_mcmc.energy_hist), required_energy)
+    mcmc.mcmc_run(
+        total_sweeps=sampling_settings["num_sweeps"],
+        sweep_size=sampling_settings["sweep_size"],
+        start_temp=sampling_settings["temperature"],
+        pot=list(calc_settings["chem_pots"].values()),
+        alpha=sampling_settings["alpha"],
+        surface=surface,
+    )
+
+    assert np.allclose(np.min(mcmc.energy_hist), required_energy)
--- tests/test_Cu.py
+++ tests/test_Cu.py
@@ -1,51 +1,94 @@
 import os
 
 import numpy as np
-from ase.calculators.lammpsrun import LAMMPS
+from catkit import Gratoms
+from catkit.gen.utils.connectivity import get_cutoff_neighbors
+from catkit.gen.utils.graph import connectivity_to_edges
 
 from mcmc import MCMC
-from mcmc.slab import initialize_slab
+from mcmc.calculators import LAMMPSRunSurfCalc
+from mcmc.slab import get_adsorption_coords, initialize_slab
+from mcmc.system import SurfaceSystem
 
+current_dir = os.path.dirname(__file__)
 
 # some regression testing first
 def test_Cu_energy():
-    required_energy = -47.30809647
+    required_energy = -36.068
 
-    # initialize some parameters first
     # Cu alat from https://www.copper.org/resources/properties/atomic_properties.html
+    element = "Cu"
     Cu_alat = 3.6147
     slab = initialize_slab(Cu_alat, size=(2, 2, 2))
 
-    element = "Cu"
-    chem_pot = 0  # chem pot 0 to less complicate things
-    alpha = 0.99  # slowly anneal
-    temp = 1  # temp in terms of kbT
-    num_sweeps = 100
-    sweep_size = 16
+    system_settings = {
+        "surface_name": "Cu(100)",
+        "cutoff": 5.0,
+        "lattice_param": {"Cu": Cu_alat},
+        "near_reduce": 0.01,
+        "planar_distance": 1.5,
+        "no_obtuse_hollow": True,
+    }
+
+    sampling_settings = {
+        "alpha": 0.99,  # slowly anneal
+        "temperature": 1.0,  # in terms of kbT
+        "num_sweeps": 50,
+        "sweep_size": 16,
+    }
+
+    calc_settings = {
+        "calc_name": "eam",
+        "optimizer": "FIRE",
+        "chem_pots": {"Cu": 0.0},
+        "relax_atoms": False,
+        "relax_steps": 100,
+        "run_dir": os.path.join(current_dir, "data/Cu_100"),
+    }
+
+    # get ads positions
+    connectivity = get_cutoff_neighbors(slab, cutoff=system_settings["cutoff"])
+    pristine_slab = slab.copy()
+
+    elem = Gratoms(element)
+    ads_positions = get_adsorption_coords(pristine_slab, elem, connectivity, debug=True)
 
     # use LAMMPS
     parameters = {"pair_style": "eam", "pair_coeff": ["* * Cu_u3.eam"]}
 
+    # set up the LAMMPS calculator
     potential_file = os.path.join(os.environ["LAMMPS_POTENTIALS"], "Cu_u3.eam")
-    lammps_calc = LAMMPS(
+    lammps_surf_calc = LAMMPSRunSurfCalc(
         files=[potential_file],
         keep_tmp_files=False,
         keep_alive=False,
+        tmp_dir=os.path.join(os.path.expanduser("~"), "tmp_files"),
     )
-    lammps_calc.set(**parameters)
+    lammps_surf_calc.set(**parameters)
 
-    # initialize object
-    Cu_mcmc = MCMC(
-        calc=lammps_calc,
+    # initialize SurfaceSystem
+    surface = SurfaceSystem(
+        slab, ads_positions, lammps_surf_calc, system_settings=system_settings
+    )
+
+    mcmc = MCMC(
+        system_settings["surface_name"],
+        calc=lammps_surf_calc,
+        canonical=False,
+        testing=False,
         element=element,
-    )
-    Cu_mcmc.mcmc_run(
-        total_sweeps=num_sweeps,
-        sweep_size=sweep_size,
-        start_temp=temp,
-        pot=chem_pot,
-        alpha=alpha,
-        slab=slab,
+        adsorbates=list(calc_settings["chem_pots"].keys()),
+        relax=calc_settings["relax_atoms"],
+        optimizer=calc_settings["optimizer"],
+    )  # no relaxation
+
+    mcmc.mcmc_run(
+        total_sweeps=sampling_settings["num_sweeps"],
+        sweep_size=sampling_settings["sweep_size"],
+        start_temp=sampling_settings["temperature"],
+        pot=list(calc_settings["chem_pots"].values()),
+        alpha=sampling_settings["alpha"],
+        surface=surface,
     )
 
-    assert np.allclose(np.min(Cu_mcmc.energy_hist), required_energy)
+    assert np.allclose(np.min(mcmc.energy_hist), required_energy)
