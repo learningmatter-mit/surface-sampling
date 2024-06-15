f56c4929d18a11ec7d5cce67aa0c2428b76968d7
Xiaochen Du <dux@outlook.sg>
2024-06-15 00:27:58 -0400

    ENH & DOC: add SwitchProposal and update docstrings

--- mcmc/events/proposal.py
+++ mcmc/events/proposal.py
@@ -5,6 +5,7 @@ from typing import Any, Dict, Iterable
 
 import numpy as np
 
+from mcmc.slab import get_complementary_idx
 from mcmc.system import SurfaceSystem
 
 logger = logging.getLogger(__name__)
@@ -52,10 +53,10 @@ class ChangeProposal(Proposal):
         self.adsorbate_list.append("None")
 
     def get_action(self) -> Dict[str, Any]:
-        """Get two indices, site1 and site2 of different elemental identities.
+        """Get an index and the elemental identity of the adsorbate to change into.
 
         Returns:
-            Dict[str, Any]: A dictionary containing the indices of the two sites and the elemental identities of the adsorbates.
+            Dict[str, Any]: A dictionary containing the index of the site and the elemental identity of the adsorbate to change into.
         """
         ads_choices = deepcopy(self.adsorbate_list)
         site_idx = np.random.choice(range(len(self.system.occ)))
@@ -78,5 +79,63 @@ class ChangeProposal(Proposal):
             "site_idx": site_idx,
             "start_ads": start_ads,
             "end_ads": end_ads,
+        }
+        return action
+
+
+class SwitchProposal(Proposal):
+    """Proposal to switch two adsorbates at different sites."""
+
+    def __init__(
+        self,
+        system: SurfaceSystem,
+        adsorbate_list: Iterable[str] = ("Sr", "O"),
+        require_per_atom_energies=False,
+        require_distance_decay=False,
+        per_atom_energies=None,
+        distance_weight_matrix=None,
+        temp=None,
+        run_folder=None,
+        plot_specific_distance_weights=False,
+        iter=None,
+    ) -> None:
+        super().__init__(system, adsorbate_list)
+        self.require_per_atom_energies = require_per_atom_energies
+        self.require_distance_decay = require_distance_decay
+        self.per_atom_energies = per_atom_energies
+        self.distance_weight_matrix = distance_weight_matrix
+        self.temp = temp
+        self.run_folder = run_folder
+        self.plot_weights = plot_specific_distance_weights
+        self.run_iter = iter
+
+    def get_action(self) -> Dict[str, Any]:
+        """Get two indices, site1 and site2 of different elemental identities.
+
+        Returns:
+            Dict[str, Any]: A dictionary containing the indices of the two sites and the elemental identities of the adsorbates.
+        """
+        logger.debug("before proposed state is")
+        logger.debug(self.system.occ)
+
+        site1_idx, site2_idx, site1_ads, site2_ads = get_complementary_idx(
+            self.system,
+            require_per_atom_energies=self.require_per_atom_energies,
+            require_distance_decay=self.require_distance_decay,
+            per_atom_energies=self.per_atom_energies,  # TODO: move to Calculator
+            distance_weight_matrix=self.distance_weight_matrix,
+            temp=self.temp,
+            ads_coords=self.system.ads_coords,
+            run_folder=self.run_folder,
+            plot_weights=self.plot_weights,
+            run_iter=self.run_iter,
+        )
+
+        action = {
+            "name": "switch",
+            "site1_idx": site1_idx,
+            "site2_idx": site2_idx,
+            "site1_ads": site1_ads,
+            "site2_ads": site2_ads,
         }
         return action
