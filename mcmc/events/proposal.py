6af7d1254715bb6071602cdc826c02045a2b1c0d
Xiaochen Du <dux@outlook.sg>
2024-06-15 16:51:42 -0400

    DOC & MAINT: add docstring to SwitchProposal, minor debugging and variable changes

--- mcmc/events/proposal.py
+++ mcmc/events/proposal.py
@@ -47,10 +47,14 @@ class ChangeProposal(Proposal):
     """
 
     def __init__(
-        self, system: SurfaceSystem, adsorbate_list: Iterable[str] = ("Sr", "O")
+        self,
+        system: SurfaceSystem,
+        adsorbate_list: Iterable[str] = ("Sr", "O"),
+        site_idx: int = None,
     ) -> None:
         super().__init__(system, adsorbate_list)
         self.adsorbate_list.append("None")
+        self.site_idx = site_idx
 
     def get_action(self) -> Dict[str, Any]:
         """Get an index and the elemental identity of the adsorbate to change into.
@@ -59,7 +63,10 @@ class ChangeProposal(Proposal):
             Dict[str, Any]: A dictionary containing the index of the site and the elemental identity of the adsorbate to change into.
         """
         ads_choices = deepcopy(self.adsorbate_list)
-        site_idx = np.random.choice(range(len(self.system.occ)))
+        if isinstance(self.site_idx, int):
+            site_idx = self.site_idx
+        else:
+            site_idx = np.random.choice(range(len(self.system.occ)))
 
         logger.debug("before proposed state is")
         logger.debug(self.system.occ)
@@ -84,20 +91,44 @@ class ChangeProposal(Proposal):
 
 
 class SwitchProposal(Proposal):
-    """Proposal to switch two adsorbates at different sites."""
+    """Proposal to switch two adsorbates at different sites.
+
+    Args:
+        system (SurfaceSystem): The surface system to propose changes to.
+        adsorbate_list (Iterable[str]): The list of adsorbates that can be proposed.
+        require_per_atom_energies (bool): Whether to require per atom energies.
+        require_distance_decay (bool): Whether to require distance decay.
+        per_atom_energies (np.ndarray): The per atom energies.
+        distance_weight_matrix (np.ndarray): The distance weight matrix.
+        temp (float): The temperature.
+        run_folder (str): The folder to run the calculations in.
+        plot_specific_distance_weights (bool): Whether to plot the specific distance weights.
+        run_iter (int): The iteration number.
+
+    Attributes:
+        adsorbate_list (List[str]): The list of adsorbates that can be proposed.
+        require_per_atom_energies (bool): Whether to require per atom energies.
+        require_distance_decay (bool): Whether to require distance decay.
+        per_atom_energies (np.ndarray): The per atom energies.
+        distance_weight_matrix (np.ndarray): The distance weight matrix.
+        temp (float): The temperature.
+        run_folder (str): The folder to run the calculations in.
+        plot_weights (bool): Whether to plot the specific distance weights.
+        run_iter (int): The iteration number.
+    """
 
     def __init__(
         self,
         system: SurfaceSystem,
         adsorbate_list: Iterable[str] = ("Sr", "O"),
-        require_per_atom_energies=False,
-        require_distance_decay=False,
-        per_atom_energies=None,
-        distance_weight_matrix=None,
-        temp=None,
-        run_folder=None,
-        plot_specific_distance_weights=False,
-        iter=None,
+        require_per_atom_energies: bool = False,
+        require_distance_decay: bool = False,
+        per_atom_energies: np.ndarray = None,
+        distance_weight_matrix: np.ndarray = None,
+        temp: float = None,
+        run_folder: str = None,
+        plot_specific_distance_weights: bool = False,
+        run_iter: int = None,
     ) -> None:
         super().__init__(system, adsorbate_list)
         self.require_per_atom_energies = require_per_atom_energies
@@ -107,7 +138,7 @@ class SwitchProposal(Proposal):
         self.temp = temp
         self.run_folder = run_folder
         self.plot_weights = plot_specific_distance_weights
-        self.run_iter = iter
+        self.run_iter = run_iter
 
     def get_action(self) -> Dict[str, Any]:
         """Get two indices, site1 and site2 of different elemental identities.
@@ -123,13 +154,20 @@ class SwitchProposal(Proposal):
             require_per_atom_energies=self.require_per_atom_energies,
             require_distance_decay=self.require_distance_decay,
             per_atom_energies=self.per_atom_energies,  # TODO: move to Calculator
-            distance_weight_matrix=self.distance_weight_matrix,
+            distance_weight_matrix=self.distance_weight_matrix,  # TODO: move to SurfaceSystem
             temp=self.temp,
             ads_coords=self.system.ads_coords,
             run_folder=self.run_folder,
             plot_weights=self.plot_weights,
             run_iter=self.run_iter,
         )
+
+        site1_coords = self.system.ads_coords[site1_idx]
+        site2_coords = self.system.ads_coords[site2_idx]
+
+        logger.debug("\n we are at iter %s", iter)
+        logger.debug("idx is %s at %s", site1_idx, site1_coords)
+        logger.debug("idx is %s at %s", site2_idx, site2_coords)
 
         action = {
             "name": "switch",
