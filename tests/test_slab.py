702f47ce0f04b082df50715de9780e258ee32e93
Xiaochen Du <dux@outlook.sg>
2024-06-20 18:07:35 -0400

    TST: add tests for test_get_complementary_idx and helper function for distance-based and energy-based weights

--- tests/test_slab.py
+++ tests/test_slab.py
@@ -1,7 +1,14 @@
+import numpy as np
 import pytest
 from ase import Atoms
 
-from mcmc.slab import change_site
+from mcmc.slab import (
+    change_site,
+    compute_boltzmann_weights,
+    get_adsorbate_indices,
+    get_complementary_idx,
+    get_complementary_idx_distance_decay,
+)
 from mcmc.system import SurfaceSystem
 
 
@@ -11,9 +18,12 @@ def system():
     atoms = Atoms(
         "GaAsGaAs", positions=[[0, 0, 0], [0, 0, 3], [1, 1, 1], [1, 1, 4]]
     )  # fake positions for now
-    ads_coords = [(0, 0, 3), (1, 1, 4), (2, 2, 5)]
-    occ = [1, 3, 0]
-    return SurfaceSystem(atoms, ads_coords, occ=occ)
+    ads_coords = [(0, 0, 3), (1, 1, 1), (2, 2, 5)]
+    occ = [1, 2, 0]
+    distance_weight_matrix = np.random.rand(3, 3)
+    return SurfaceSystem(
+        atoms, ads_coords, occ=occ, distance_weight_matrix=distance_weight_matrix
+    )
 
 
 def test_change_site_with_existing_adsorbate(system):
@@ -49,3 +59,105 @@ def test_change_site_with_invalid_site_index(system):
     # Try to change an adsorbate at an invalid site index
     with pytest.raises(IndexError):
         change_site(system, 5, "As")
+
+
+def test_get_adsorbate_indices(system):
+    adsorbates = get_adsorbate_indices(system)
+    assert adsorbates == {"As": [0], "Ga": [1], "None": [2]}
+
+
+def test_compute_boltzmann_weights(system):
+    # Set up the test case
+    per_atom_energies = [1.0, 0.5, 1.0, 0.6]
+    temperature = 1.0
+    curr_ads = get_adsorbate_indices(system)
+
+    weights = compute_boltzmann_weights(
+        system,
+        per_atom_energies=per_atom_energies,
+        temperature=temperature,
+        curr_ads=curr_ads,
+    )
+    # Perform assertions
+    assert len(weights) == 3
+    assert all(val > 0 for val in weights.values())
+    assert np.allclose(
+        sorted(weights.values()),
+        sorted(
+            {
+                "As": [0.1850956],
+                "Ga": [0.30517106],
+                "None": [1],
+            }.values()
+        ),
+    )
+
+
+def test_get_complementary_idx_distance_decay(system):
+    # Set up the test case
+    curr_ads = get_adsorbate_indices(system)
+    type1 = "As"
+    type2 = "Ga"
+    weights1 = np.array([0.1850956])
+    weights2 = np.array([0.30517106])
+    plot_weights = False
+    run_folder = "."
+    run_iter = 0
+
+    # Call the function
+    site1_idx, site2_idx = get_complementary_idx_distance_decay(
+        system,
+        curr_ads,
+        type1,
+        type2,
+        weights1,
+        weights2,
+        plot_weights,
+        run_folder,
+        run_iter,
+    )
+
+    # Perform assertions
+    assert site1_idx != site2_idx
+    if system.occ[site1_idx] != 0:
+        assert type1 in system.real_atoms[system.occ[site1_idx]].symbol
+    else:
+        assert type1 == "None"
+    if system.occ[site2_idx] != 0:
+        assert type2 in system.real_atoms[system.occ[site2_idx]].symbol
+    else:
+        assert type2 == "None"
+
+
+def test_get_complementary_idx(system):
+    # Set up the test case
+    require_per_atom_energies = False
+    require_distance_decay = False
+    per_atom_energies = None
+    temperature = 1.0
+    plot_weights = False
+    run_folder = "."
+    run_iter = 0
+
+    # Call the function
+    site1_idx, site2_idx, type1, type2 = get_complementary_idx(
+        system,
+        require_per_atom_energies,
+        require_distance_decay,
+        per_atom_energies,
+        temperature,
+        plot_weights,
+        run_folder,
+        run_iter,
+    )
+
+    # Perform assertions
+    assert site1_idx != site2_idx
+    if system.occ[site1_idx] != 0:
+        assert type1 in system.real_atoms[system.occ[site1_idx]].symbol
+    else:
+        assert type1 == "None"
+    if system.occ[site2_idx] != 0:
+        assert type2 in system.real_atoms[system.occ[site2_idx]].symbol
+    else:
+        assert type2 == "None"
