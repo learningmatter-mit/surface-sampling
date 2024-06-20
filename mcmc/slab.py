930af4cd72ec206b05778732d9a9cec5b2890be9
Xiaochen Du <dux@outlook.sg>
2024-06-20 18:05:20 -0400

    MAINT: refactor get_complementary_idx in slab.py

--- mcmc/slab.py
+++ mcmc/slab.py
@@ -58,11 +58,145 @@ def initialize_slab(
     return catkit_slab
 
 
+def get_adsorbate_indices(surface: SurfaceSystem) -> dict[str, list[int]]:
+    """Get a dictionary of adsorbate indices grouped by elemental identity.
+
+    Args:
+        surface (SurfaceSystem): The surface system.
+
+    Returns:
+        dict: A dictionary of adsorbate indices grouped by elemental identity.
+    """
+    adsorbed_idx = surface.filled_occ_idx
+    # Group adsorbates present in slab
+    adsorbates = {
+        k: list(g)
+        for k, g in itertools.groupby(
+            adsorbed_idx, key=lambda x: surface.real_atoms[surface.occ[x]].symbol
+        )
+    }
+    # Add virtual sites only if there are empty ones
+    empty_idx = surface.empty_occ_idx
+    if len(empty_idx) > 0:
+        adsorbates["None"] = empty_idx
+
+    return adsorbates
+
+
+def choose_adsorbate_type(adsorbates: dict) -> tuple[str, str]:
+    """Choose two types of adsorbates from the given dictionary.
+
+    Args:
+        adsorbates (dict): A dictionary of adsorbate indices grouped by elemental identity.
+
+    Returns:
+        tuple: A tuple containing the two chosen adsorbate types.
+    """
+    types = list(adsorbates.keys())
+    type1, type2 = random.sample(types, 2)
+    return type1, type2
+
+
+def compute_boltzmann_weights(
+    surface: SurfaceSystem,
+    per_atom_energies: Union[list, np.ndarray],
+    temperature: float,
+    curr_ads: dict[str, list],
+) -> dict[str, np.ndarray]:
+    """Compute the Boltzmann weights for each adsorbate type.
+
+    Args:
+        surface (SurfaceSystem): The surface system.
+        per_atom_energies (Union[list, np.ndarray]): The per atom energies in eV.
+        temperature (float): The temperature in eV.
+        curr_ads (dict): A dictionary of adsorbate indices grouped by elemental identity.
+
+    Returns:
+        dict: A dictionary containing the Boltzmann weights for each adsorbate type.
+
+    Raises:
+        ValueError: If require_per_atom_energies is True, but no per_atom_energies were provided.
+    """
+    if per_atom_energies is None:
+        raise ValueError(
+            "require_per_atom_energies is True, but no per_atom_energies were provided"
+        )
+    logger.debug("in `get_complementary_idx` using per atom energies")
+    logger.debug("per atom energies are %s", per_atom_energies)
+    logger.debug("temperature is %s", temperature)
+    boltzmann_weights = softmax(np.array(per_atom_energies) / temperature)
+    logger.debug("boltzmann weights are %s", boltzmann_weights)
+
+    # create weight dict for each adsorbate except empty sites
+    weights = {
+        k: boltzmann_weights[surface.occ[v]] if k != "None" else np.ones_like(v)
+        for k, v in curr_ads.items()
+    }
+    return weights
+
+
+def get_complementary_idx_distance_decay(
+    surface: SurfaceSystem,
+    curr_ads: dict[str, list],
+    type1: str,
+    type2: str,
+    weights1: np.ndarray,
+    weights2: np.ndarray,
+    plot_weights: bool,
+    run_folder: str,
+    run_iter: int,
+) -> tuple[int, int]:
+    """Get two indices, site1 and site2 of different elemental identities using distance decay.
+
+    Args:
+        surface (SurfaceSystem): The surface system.
+        curr_ads (dict): A dictionary of adsorbate indices grouped by elemental identity.
+        type1 (str): The first adsorbate type.
+        type2 (str): The second adsorbate type.
+        weights1 (np.ndarray): The weights for the first adsorbate type.
+        weights2 (np.ndarray): The weights for the second adsorbate type.
+        plot_weights (bool): Whether to plot the specific distance weights.
+        run_folder (str): The folder to save the plots in.
+        run_iter (int): The iteration number.
+
+    Returns:
+        Tuple[int, int]: A tuple containing the indices of the two sites.
+
+    Raises:
+        ValueError: If require_distance_decay is True, but no distance_weight_matrix was provided.
+    """
+    if surface.distance_weight_matrix is None:
+        raise ValueError(
+            "require_distance_decay is True, but no distance_weight_matrix was provided"
+        )
+
+    # TODO: run_folder and run_iter should be saved to the SurfaceSystem
+    logger.debug("Length of curr_ads[type1]: %s", len(curr_ads[type1]))
+    logger.debug("Length of weights1: %s", len(weights1))
+    site1_idx = random.choices(curr_ads[type1], weights=weights1, k=1)[0]
+    specific_distance_weights = surface.distance_weight_matrix[site1_idx]
+    if plot_weights:
+        plot_specific_weights(
+            surface.ads_coords,
+            specific_distance_weights,
+            site1_idx,
+            save_folder=run_folder,
+            run_iter=run_iter,
+        )
+    combined_type2_weights = weights2 * specific_distance_weights[curr_ads[type2]]
+    site2_idx = random.choices(curr_ads[type2], weights=combined_type2_weights, k=1)[0]
+    return site1_idx, site2_idx
+
+
 def get_complementary_idx(
     surface: SurfaceSystem,
     require_per_atom_energies: bool = False,
     require_distance_decay: bool = False,
-    **kwargs,
+    per_atom_energies: np.ndarray = None,
+    temperature: float = 1.0,
+    plot_weights: bool = False,
+    run_folder: str = ".",
+    run_iter: int = 0,
 ) -> tuple[int, int, str, str]:
     """Get two indices, site1 and site2 of different elemental identities.
 
@@ -70,108 +204,56 @@ def get_complementary_idx(
         surface (SurfaceSystem): The surface system to propose changes to.
         require_per_atom_energies (bool): Whether to use per atom energies.
         require_distance_decay (bool): Whether to use distance decay.
+        per_atom_energies (np.ndarray): The per atom energies in eV.
+        temperature (float): The temperature in eV.
+        plot_weights (bool): Whether to plot the specific distance weights.
+        run_folder (str): The folder to save the plots in.
+        run_iter (int): The iteration number.
 
     Returns:
         Tuple[int, int, str, str]: A tuple containing the indices of the two sites and the elemental identities of the adsorbates.
     """
-    # TODO: split into two functions
-    # find indices of adsorbed atoms
-    adsorbed_idx = np.argwhere(surface.occ != 0).flatten()
-
-    per_atom_energies = kwargs.get("per_atom_energies", None)
-    distance_weight_matrix = kwargs.get("distance_weight_matrix", None)
-    # logger.info(f"distance weight matrix has shape {distance_weight_matrix.shape}")
-    # group adsorbates present in slab
-    curr_ads = {
-        k: list(g)
-        for k, g in itertools.groupby(
-            adsorbed_idx, key=lambda x: surface.real_atoms[surface.occ[x]].symbol
-        )
-    }
-    # add empty sites
-    # find indices of empty sites
-    empty_idx = np.argwhere(surface.occ == 0).flatten().tolist()
-    curr_ads["None"] = empty_idx
-    # complelety populate the adsorbates including empty sites
-    logger.debug(f"current ads {curr_ads}")
+    curr_ads = get_adsorbate_indices(surface)
+    logger.debug("current ads %s", curr_ads)
 
     if require_per_atom_energies:
-        if per_atom_energies is None:
-            raise ValueError(
-                "require_per_atom_energies is True, but no per_atom_energies were provided"
-            )
-        logger.debug("in `get_complementary_idx` using per atom energies")
-        logger.debug("per atom energies are %s", per_atom_energies)
-        # TODO might want to change the "temperature"
-        # temp = kwargs.get("temp", 0.5)  # in terms of eV
-        temp = 1  # fixed at 1 eV
-        logger.debug("temp is %s", temp)
-        boltzmann_weights = softmax(per_atom_energies / temp)
-        logger.debug("boltzmann weights are %s", boltzmann_weights)
-
-        # create weight dict for each adsorbate except empty sites
-        weights = {
-            k: boltzmann_weights[surface.occ[v]] if k != "None" else np.ones_like(v)
-            for k, v in curr_ads.items()
-        }
+        weights = compute_boltzmann_weights(
+            surface, per_atom_energies, temperature, curr_ads
+        )
     else:
-        # all uniform weights
+        # Uniform weights
         weights = {k: np.ones_like(v) for k, v in curr_ads.items()}
 
-    # choose two types
-    types = list(curr_ads.keys())
-    type1, type2 = random.sample(types, 2)
-
+    # Randomly pick two adsorbate types
+    type1, type2 = choose_adsorbate_type(curr_ads)
+    weights1, weights2 = weights[type1], weights[type2]
     # Checking if the weights are valid, and if not, replace them with an array of ones.
-    weights1 = (
-        weights[type1] if weights[type1].any() > 0 else np.ones(len(curr_ads[type1]))
-    )
-    weights2 = (
-        weights[type2] if weights[type2].any() > 0 else np.ones(len(curr_ads[type2]))
-    )
+    # weights1 = (
+    #     weights[type1] if weights[type1].any() > 0 else np.ones(len(curr_ads[type1]))
+    # )
+    # weights2 = (
+    #     weights[type2] if weights[type2].any() > 0 else np.ones(len(curr_ads[type2]))
+    # )
 
     if require_distance_decay:
-        # TODO merge with per atom energies
-        if distance_weight_matrix is None:
-            raise ValueError(
-                "require_distance_decay is True, but no distance_weight_matrix was provided"
-            )
-        logger.debug("in `get_complementary_idx` using distance decay")
-        # get random idx for type 1 first
-        # weights_type1 = np.ones(len(curr_ads[type1]))
-        # weights_type1_new = weights[type1] if weights[type1].any() > 0 else np.ones(len(curr_ads[type1]))
-        site1_idx = random.choices(curr_ads[type1], weights=weights1, k=1)[
-            0
-        ]  # even weights
-
-        ads_coords = kwargs.get("ads_coords", None)
-        specific_distance_weights = distance_weight_matrix[
-            site1_idx
-        ]  # get the weights for the second type
-        logger.debug("specific weights shape is %s", specific_distance_weights.shape)
-        if kwargs.get("plot_weights", False):
-            logger.debug("plotting weights")
-            plot_specific_weights(
-                ads_coords,
-                specific_distance_weights,
-                site1_idx,
-                save_folder=kwargs.get("run_folder", "."),
-                run_iter=kwargs.get("run_iter", 0),
-            )
-        combined_type2_weights = (
-            weights2 * specific_distance_weights[curr_ads[type2]]
-        )  # energy based weights * distance decay weights
-        site2_idx = random.choices(
-            curr_ads[type2], weights=combined_type2_weights, k=1
-        )[
-            0
-        ]  # weighted by distance decay
+        site1_idx, site2_idx = get_complementary_idx_distance_decay(
+            surface,
+            curr_ads,
+            type1,
+            type2,
+            weights1,
+            weights2,
+            plot_weights,
+            run_folder,
+            run_iter,
+        )
     else:
         # get random idx belonging to those types
         site1_idx, site2_idx = [
             random.choices(curr_ads[x], weights=w, k=1)[0]
             for x, w in zip([type1, type2], [weights1, weights2])
         ]
+
     slab_idx_1, slab_idx_2 = surface.occ[site1_idx], surface.occ[site2_idx]
     logger.debug("type1 %s, type2 %s", type1, type2)
     logger.debug("site1_idx %s, site2_idx %s", slab_idx_1, slab_idx_2)
