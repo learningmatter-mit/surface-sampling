09d0e8ceabc66967d2d8c7cbb7b4bddfc10e3184
Xiaochen Du <dux@outlook.sg>
2024-07-01 15:13:43 -0400

    ENH: generalize `sample_surface_PaiNN.py` to `sample_surface.py`
    - support config.json files for system, sampling, and calc settings
    - supports pre-trained CHGNet, MACE, and trained PaiNN
    - `sample_bulk.py` should be similar but WIP

--- scripts/sample_bulk.py
+++ scripts/sample_bulk.py
@@ -1,4 +1,4 @@
-"""Script to sample SrTiO3 surface using VSSR-MC."""
+"""Script to sample SrIrO3 bulk system using VSSR-MC."""
 
 import argparse
 import json
@@ -8,12 +8,9 @@ from copy import deepcopy
 from pathlib import Path
 from time import perf_counter
 
-import numpy as np
-from ase.constraints import FixAtoms
 from nff.io.ase_calcs import NeuralFF
+from nff.nn.models.chgnet import CHGNetNFF
 from nff.utils.cuda import cuda_devices_sorted_by_free_mem
-from pymatgen.analysis.adsorption import AdsorbateSiteFinder
-from pymatgen.io.ase import AseAtomsAdaptor
 
 from mcmc import MCMC
 from mcmc.calculators import EnsembleNFFSurface
@@ -23,34 +20,30 @@ from mcmc.utils.misc import get_atoms_batch
 logger = logging.getLogger("mcmc")
 logger.setLevel(logging.INFO)
 
-np.set_printoptions(precision=3, suppress=True)
+# TODO: follow from sample_pourbaix_surface.py in pourbaix branch
 
 
 def parse_args():
     """Parse command line arguments."""
-    parser = argparse.ArgumentParser(description="Perform mcmc on surfaces.")
+    parser = argparse.ArgumentParser(description="Perform VSSR-MC sampling")
     parser.add_argument(
-        "--surface_name",
+        "--starting_structure",
         type=str,
-        default="SrTiO3_001_2x2",
-        help="name of the surface",
-    )
-    parser.add_argument(
-        "--starting_structure_path",
-        type=str,
+        default="data/SrIrO3_2x2x2_bulk.pkl",
         help="path to the starting structure",
-    )
-    parser.add_argument(
-        "--model_paths",
-        type=str,
-        nargs="+",
-        help="paths to the models",
     )
     parser.add_argument(
         "--save_folder",
         type=Path,
         default="./",
-        help="Folder to output.",
+        help="Folder to save sampled surfaces.",
+    )
+    parser.add_argument(
+        "--nnids",
+        type=int,
+        nargs="+",
+        default=[2537, 2538, 2539],
+        help="ids of the nnpotentials to use",
     )
     parser.add_argument(
         "--chem_pot",
@@ -61,12 +54,6 @@ def parse_args():
     )
     parser.add_argument("--sweeps", type=int, default=100, help="MCMC sweeps")
     parser.add_argument("--sweep_size", type=int, default=50, help="MCMC sweep size")
-    parser.add_argument(
-        "--alpha",
-        type=float,
-        default=1.0,
-        help="annealing parameter, fraction to multiply with temperature",
-    )
     parser.add_argument("--temp", type=float, default=1.0, help="temperature in kbT")
     parser.add_argument("--relax", action="store_true", help="perform relaxation for the steps")
     parser.add_argument("--relax_steps", type=int, default=5, help="max relaxation steps")
@@ -75,9 +62,9 @@ def parse_args():
     )
     parser.add_argument("--offset", action="store_true", help="whether to use energy offsets")
     parser.add_argument(
-        "--offset_data_path",
+        "--offset_data",
         type=str,
-        default="data/nff/offset_data.json",
+        default="/mnt/data0/dux/surf_samp_working/SrIrO3/data/nff/chgnet_offset_data_eV.json",
     )
     parser.add_argument(
         "--device",
@@ -90,13 +77,12 @@ def parse_args():
 
 
 def main(
-    surface_name: str,
-    starting_structure_path: Path | str,
-    model_paths: list[str],
+    starting_structure: Path | str,
+    save_folder: Path,
+    nnids: list[int],
     chem_pot: list[float],
     sweeps: int,
     sweep_size: int,
-    alpha: float,
     temp: float,
     relax: bool,
     offset_data_path: str,
@@ -104,18 +90,16 @@ def main(
     record_interval: int = False,
     offset: bool = False,
     device: str = "cuda",
-    save_folder: str = "./",
 ):
-    """Perform VSSR-MC sampling for surfaces.
+    """Perform VSSR-MC sampling for SrIrO3 bulk system.
 
     Args:
-        surface_name (str): name of the surface
-        starting_structure_path (Union[Path, str]): path to the starting structure
-        model_paths (List[str]): paths to the models
-        chem_pot (List[float]): chemical potential for each element
+        starting_structure (Path | str): path to the starting structure
+        save_folder (Path): Folder to save sampled surfaces.
+        nnids (list[int]): ids of the nnpotentials to use
+        chem_pot (list[float]): chemical potential for each element
         sweeps (int): MCMC sweeps
-        sweep_size (int): MCMC sweep size
-        alpha (float): alpha for MCMC
+        sweep_size (int): MCMC steps (iterations) per sweep
         temp (float): temperature in kbT
         relax (bool): perform relaxation for the steps
         offset_data_path (str): path to the offset data
@@ -123,13 +107,11 @@ def main(
         record_interval (int, optional): save interval for relaxation. Defaults to False.
         offset (bool, optional): whether to use energy offsets. Defaults to False.
         device (str, optional): device to use for calculations. Defaults to "cuda".
-        save_folder (Path): Folder to output
     """
     save_path = Path(save_folder)
     save_path.mkdir(parents=True, exist_ok=True)
 
     DEVICE = f"cuda:{cuda_devices_sorted_by_free_mem()[-1]}" if device == "cuda" else "cpu"
-
     try:
         with open(offset_data_path, "r") as f:
             offset_data = json.load(f)
@@ -137,25 +119,17 @@ def main(
         logger.error("Could not find offset data at %s", offset_data_path)
         raise e
 
-    with open(starting_structure_path, "rb") as f:
-        starting_slab = pickle.load(f)
-
-    chem_symbols = starting_slab.get_chemical_symbols()
-    elements = list(set(chem_symbols))
-
-    print(f"Elements: {elements}")
-
     system_settings = {
-        "surface_name": surface_name,
+        "surface_name": "SrIrO3_bulk",
         "cutoff": 5.0,
         "device": DEVICE,
-        "near_reduce": 0.01,
-        "planar_distance": 1.55,
-        "no_obtuse_hollow": True,
+        # "near_reduce": 0.01,
+        # "planar_distance": 1.55,
+        # "no_obtuse_hollow": True,
     }
 
     sampling_settings = {
-        "alpha": alpha,  # no annealing
+        "alpha": 1.0,  # no annealing
         "temperature": temp,  # in terms of kbT, 1000 K
         "num_sweeps": sweeps,
         "sweep_size": sweep_size,
@@ -163,102 +137,96 @@ def main(
 
     calc_settings = {
         "calc_name": "NFF",
-        "optimizer": "FIRE",
-        "chem_pots": dict(zip(elements, chem_pot, strict=False)),
+        "chem_pots": {
+            "Sr": chem_pot[0],
+            "Ir": chem_pot[1],
+            "O": chem_pot[2],
+        },
+        "offset_data": offset_data,
+        "optimizer": "BFGS",
         "relax_atoms": relax,
         "relax_steps": relax_steps,
         "record_interval": record_interval,  # record structure every n steps
         "offset": offset,
-        "offset_data": offset_data,
     }
 
-    # Obtain adsorption sites
-    pristine_slab = starting_slab.copy()
-    pristine_pmg_slab = AseAtomsAdaptor.get_structure(pristine_slab)
-    site_finder = AdsorbateSiteFinder(pristine_pmg_slab)
+    # open 2x2x2 cubic from file
+    with open(starting_structure, "rb") as f:
+        cubic_cell_2x2x2 = pickle.load(f)
 
-    ads_positions = site_finder.find_adsorption_sites(
-        put_inside=True,
-        symm_reduce=False,
-        near_reduce=system_settings["near_reduce"],
-        distance=system_settings["planar_distance"],
-        no_obtuse_hollow=system_settings["no_obtuse_hollow"],
-    )["all"]
+    chem_symbols = cubic_cell_2x2x2.get_chemical_symbols()
+    print(f"Chemical symbols: {chem_symbols}")
 
-    print(f"adsorption coordinates are: {ads_positions[:5]}...")
+    # Use Pretrained NFF model
+    chgnet_nff = CHGNetNFF.load(device=system_settings["device"])
 
-    # Load Ensemble NFF Model
-    # requires an ensemble of models in this path and an `offset_data.json` file
-    models = []
-    for modeldir in model_paths:
-        m = NeuralFF.from_file(modeldir, device=DEVICE).model
-        models.append(m)
+    nff_calc = NeuralFF(
+        chgnet_nff,
+        device=system_settings["device"],
+        model_units="eV/atom",
+        prediction_units="eV",
+    )
 
     nff_surf_calc = EnsembleNFFSurface(
-        models,
-        device=DEVICE,
+        [nff_calc.model],
+        device=system_settings["device"],
+        model_units="eV/atom",
+        prediction_units="eV",
+        offset_units="eV",
     )
     nff_surf_calc.set(**calc_settings)
 
-    # Use Pretrained NFF model
-    # chgnet_nff = CHGNetNFF.load(device=system_settings["device"])
-
-    # nff_calc = NeuralFF(
-    #     chgnet_nff,
-    #     device=system_settings["device"],
-    #     model_units="eV/atom",
-    #     prediction_units="eV",
-    # )
-
-    # nff_surf_calc = EnsembleNFFSurface(
-    #     [nff_calc.model],
-    #     device=system_settings["device"],
-    #     model_units="eV/atom",
-    #     prediction_units="eV",
-    #     offset_units="eV",
-    # )
-    # nff_surf_calc.set(**calc_settings)
-
     # Initialize SurfaceSystem (actually bulk system)
+    ads_positions = cubic_cell_2x2x2.get_positions()
+    ads_idx = list(range(len(ads_positions)))
+
+    # remove the first ads site due to 0 index
+    # 0 index will always be thought of as being empty
+    # TODO might have to fix this
+    ads_positions = ads_positions[1:]
+    ads_idx = ads_idx[1:]
+
+    # set attributes
     slab_batch = get_atoms_batch(
-        pristine_slab,
+        cubic_cell_2x2x2,
         nff_cutoff=system_settings["cutoff"],
         device=system_settings["device"],
         props={"energy": 0, "energy_grad": []},  # needed for NFF
     )
+    # slab_batch = AtomsBatch(
+    #     cubic_cell_2x2x2,
+    #     cutoff=system_settings["cutoff"],
+    #     props={"energy": 0, "energy_grad": []},
+    #     calculator=nff_surf_calc,
+    #     requires_large_offsets=True,
+    #     directed=True,
+    #     device=DEVICE,
+    # )
 
-    # Fix atoms in the bulk
-    num_bulk_atoms = len(slab_batch)
-    bulk_indices = list(range(num_bulk_atoms))
-    print(f"bulk indices {bulk_indices}")
-    surf_indices = pristine_slab.get_surface_atoms()
+    # Fake as adsorbate slab
+    slab_batch.set_tags([1] * len(slab_batch))
+    print(f"Tags are {slab_batch.get_tags()}")
 
-    fix_indices = list(set(bulk_indices) - set(surf_indices))
-    print(f"fix indices {fix_indices}")
-
-    c = FixAtoms(indices=fix_indices)
-    slab_batch.set_constraint(c)
-
-    surface = SurfaceSystem(
-        slab_batch, ads_coords=ads_positions, calc=nff_surf_calc, system_settings=system_settings
+    bulk = SurfaceSystem(
+        slab_batch,
+        calc=nff_surf_calc,
+        ads_coords=ads_positions,
+        occ=ads_idx,
+        system_settings=system_settings,
     )
-    starting_atoms_path = save_path / "all_virtual_ads.cif"
-    print(f"Saving surface with virtual atoms to {starting_atoms_path}")
-    surface.all_atoms.write(starting_atoms_path)
+    bulk.all_atoms.write(save_path / "SrTiO3_2x2_bulk_starting.cif")
 
-    print(f"Starting chemical formula: {slab_batch.get_chemical_formula()}")
+    print(f"Starting chemical formula {slab_batch.get_chemical_formula()}")
 
-    print(f"NFF calc starting energy: {surface.get_potential_energy()} eV")
-
-    print(f"Starting surface energy: {surface.get_surface_energy()} eV")
+    print(f"NFF calc predicts {nff_calc.get_potential_energy(slab_batch)} eV")
 
     print(f"Offset units: {nff_surf_calc.offset_units}")
 
     # Do different bulk defect sampling for the 2x2x2 cell
     # Sample across chemical potentials
-    starting_surface = deepcopy(surface)
+    starting_bulk = deepcopy(bulk)
 
-    # Perform MCMC and view results
+    # Perform MCMC and view results.
     mcmc = MCMC(
         system_settings["surface_name"],
         calc=nff_surf_calc,
@@ -281,7 +249,7 @@ def main(
         start_temp=sampling_settings["temperature"],
         pot=list(calc_settings["chem_pots"].values()),
         alpha=sampling_settings["alpha"],
-        surface=starting_surface,
+        surface=starting_bulk,
         run_folder=save_path,
     )
     stop = perf_counter()
@@ -308,19 +276,17 @@ def main(
 if __name__ == "__main__":
     args = parse_args()
     main(
-        args.surface_name,
-        args.starting_structure_path,
-        args.model_paths,
+        args.starting_structure,
+        args.save_folder,
+        args.nnids,
         args.chem_pot,
         args.sweeps,
         args.sweep_size,
-        args.alpha,
         args.temp,
         args.relax,
-        args.offset_data_path,
+        args.offset_data,
         args.relax_steps,
         args.record_interval,
         args.offset,
         args.device,
-        save_folder=args.save_folder,
     )
