b3b5c8eb553bfdf4d4c84f86e63e6ff5af9536e9
Xiaochen Du <dux@outlook.sg>
2025-04-13 14:34:12 -0400

    MAINT: update tutorials and tests

--- tests/events/test_proposal.py
+++ tests/events/test_proposal.py
@@ -1,10 +1,10 @@
+"""Unit tests for the proposal module."""
+
 import random
 
 import numpy as np
-import pytest
 
 from mcmc.events.proposal import ChangeProposal, SwitchProposal
-from tests.events.test_fixtures import system
 
 SEED = 11
 # Set the seed for the random module
@@ -15,6 +15,7 @@ np.random.seed(SEED)
 
 
 def test_change_proposal_get_action(system):
+    """Test the get_action method of the ChangeProposal class."""
     # Create a ChangeProposal object
     adsorbate_list = ("Ga", "As")
     proposal = ChangeProposal(system, adsorbate_list)
@@ -38,6 +39,7 @@ def test_change_proposal_get_action(system):
 
 
 def test_adsorb_at_empty_site(system):
+    """Test the get_action method of the ChangeProposal class at empty sites."""
     # Create a ChangeProposal object
     adsorbate_list = ("Ga", "As")
     proposal = ChangeProposal(system, adsorbate_list)
@@ -56,6 +58,7 @@ def test_adsorb_at_empty_site(system):
 
 
 def test_change_at_adsorbed_site(system):
+    """Test the get_action method of the ChangeProposal class at adsorbed sites."""
     # Create a ChangeProposal object
     adsorbate_list = ("Ga", "As")
     proposal = ChangeProposal(system, adsorbate_list)
@@ -66,13 +69,14 @@ def test_change_at_adsorbed_site(system):
     action = proposal.get_action()
 
     # Assert that the start_ads value is a valid adsorbate
-    assert action["start_ads"] in set(proposal.adsorbate_list) - set("None")
+    assert str(action["start_ads"]) in set(proposal.adsorbate_list) - set("None")
 
     # Assert that the end_ads value is a valid adsorbate
     assert action["end_ads"] in set(proposal.adsorbate_list)
 
 
 def test_switch_at_adsorbed_sites_simple(system):
+    """Test the get_action method of the SwitchProposal class."""
     # None of the distance-based or energy-based proposal
 
     # Create a SwitchProposal object
